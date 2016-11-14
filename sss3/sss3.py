b48ba98a3b481f9bd9c28b4598560fe6dcfef4e6
alen slejko <alen.slejko@gmail.com>
2016-11-14 19:55:13 +0100

    no message

--- sss3/sss3.py
+++ sss3/sss3.py
@@ -1,13 +1,29 @@
 import sys
+import os
 import boto3
+import ctypes
 
-commands = ['init', 'i', 'status', 's', 'clone', 'pull', 'push', 'commit', 'c', 'help', 'h']
+
+commands = ['init', 'i', 'status', 's', 'clone', 'pull', 'push', 'commit', 'c', 'help', 'h', 'config']
+fname="sss3"
+
+
 
 def help():
     print('Help...')
     return
 
 def init():
+    if not os.path.isdir(fname):
+        if os.name == 'nt':
+            os.makedirs(fname)
+            ctypes.windll.kernel32.SetFileAttributesW(ur""+fname,0x02)
+        else:
+            os.makedirs("."+fname)
+
+
+
+
     return
 
 def status():
@@ -17,13 +33,18 @@ def clone():
     return
 
 def pull():
-    return\
+    return
 
 def push():
     return
 
 def commit():
     return
+
+def config():
+    print("to je config")
+
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -36,5 +57,9 @@ if __name__ == '__main__':
         quit()
 
     print("Command: %s" % cmd)
+
     if cmd == 'init' or cmd == 'i':
         init()
+
+    if cmd == 'config':
+        config()
