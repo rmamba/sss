31bd780145eb77e06ee80909bfd909c98e663d6f
alen slejko <alen.slejko@gmail.com>
2016-11-21 20:28:54 +0100

    Config created

--- sss3/sss3.py
+++ sss3/sss3.py
@@ -1,13 +1,17 @@
 import sys,json
 import os
+from StringIO import StringIO
+
 import boto3
 import ctypes
+import ConfigParser
 
 
 commands = ['init', 'i', 'status', 's', 'clone', 'pull', 'push', 'commit', 'c', 'help', 'h', 'config']
 fname="sss3"
 jsonpath="./"+fname+"/config.json"
-
+awspath=os.path.expanduser('~')+"\\aws"
+os.environ["AWS_SHARED_CREDENTIALS_FILE"]=awspath+"\\credentials"
 
 
 def help():
@@ -30,14 +34,8 @@ def init():
 
 
     if len(sys.argv) == 5:
-        #Insert new user into json file if doesnt exists - overwrite if exists
-        with open(jsonpath, 'r') as outfile:
-            try:
-                data=json.load(outfile)
-            except:
-                data={}
-
-        data[sys.argv[2]]={"Secret_Access_Key": sys.argv[3], "Access_Key_ID": sys.argv[4]}
+        #Insert data into json
+        data={"GUID":sys.argv[2],"Secret_Access_Key": sys.argv[3], "Access_Key_ID": sys.argv[4]}
         with open(jsonpath, 'w') as outfile:
             json.dump(data, outfile)
 
@@ -65,7 +63,58 @@ def commit():
     return
 
 def config():
-    print("to je config")
+    write=False;
+    if len(sys.argv)>5:
+        print "Error:Too many arguments"
+
+    else:
+        #check if global config location exists if not create it
+        if not os.path.isdir(awspath):
+            if os.name == 'nt':
+                os.makedirs(awspath)
+                ctypes.windll.kernel32.SetFileAttributesW(ur""+awspath,0x02)
+            else:
+                os.makedirs("."+awspath)
+
+        parser = ConfigParser.ConfigParser()
+        parser.readfp(StringIO('[default]'))
+        parser.set('default', 'aws_access_key_id', sys.argv[3])
+        parser.set('default', 'aws_secret_access_key', sys.argv[4])
+
+        if os.path.isfile(awspath+ "\\credentials"):
+            if os.stat(awspath+ "\\credentials").st_size != 0:
+                dec=raw_input("Are you sure to overwrite credentials? Type y for yes and n  for no")
+                if dec.startswith('y'):
+                    write=True;
+                elif dec.startswith('n'):
+                    print "Exiting without saving credentials..."
+                    exit(0)
+            else:
+                write=True
+        else:
+            write=True
+
+        if write:
+            print "Saving configuration to global config file"
+            with open(awspath + "\\credentials", 'w') as configfile:
+                parser.write(configfile)
+
+
+    #s3 = boto3.resource('s3')
+    #bucket = s3.Bucket('sss3-test')
+    #for obj in bucket.objects.all():
+    #    print(obj)
+
+
+    return ""
+
+
+def read_config(section,path):
+    config = ConfigParser.RawConfigParser()
+    config.read(path)
+    return config.items(section)
+
+
 
 
 
