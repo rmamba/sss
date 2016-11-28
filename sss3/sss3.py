cb427c5fe65f2a7ed788f4a4f398bb124ce3f132
alen slejko <alen.slejko@gmail.com>
2016-11-28 14:54:10 +0100

    Config finished

--- sss3/sss3.py
+++ sss3/sss3.py
@@ -1,9 +1,9 @@
 import sys
 import json
 import os
-
 import boto3
 import ctypes
+
 
 class SSS3:
     aws_access_key_id = None
@@ -19,22 +19,22 @@ class SSS3:
     def __read_config(self):
         with open(self.CONFIG_FILE) as json_data:
             self.config = json.load(json_data)
+        return self.config
 
     # Check if connection to bucket is succesfull
-    def __check_connection_to_bucket(accesid, secretkey, bucketname):
+    def __check_connection_to_bucket(self, accesid, secretkey, bucketname):
         connection_to_bucket = True
         session = boto3.Session(aws_access_key_id=accesid, aws_secret_access_key=secretkey)
         s3 = session.resource('s3')
         bucket = s3.Bucket(bucketname)
-
         try:
             s3.meta.client.head_bucket(Bucket=bucketname)
         except:
             connection_to_bucket = False
         return connection_to_bucket
 
+    # If directory sss3 doesnt exist
     def __check_hidden_folder_exists(self):
-        # If directory sss3 doesnt exist
         if not os.path.isdir(self.FOLDER_NAME):
             if os.name == 'nt':
                 os.makedirs(self.FOLDER_NAME)
@@ -44,8 +44,9 @@ class SSS3:
             open("./" + self.FOLDER_NAME + "/config.json", 'w').close()
 
         else:
-            if not os.path.isfile(self.FOLDER_NAME):
-                open(self.FOLDER_NAME, 'w').close()
+            if not os.path.isfile(self.CONFIG_FILE):
+                open(self.CONFIG_FILE, 'w').close()
+
 
     def __init__(self, arguments):
         self.arguments = arguments
@@ -59,7 +60,6 @@ class SSS3:
             self.__help()
             return
 
-        print("Command: %s" % cmd)
 
         if cmd == 'init' or cmd == 'i':
             self.__init()
@@ -67,21 +67,20 @@ class SSS3:
         if cmd == 'config':
             self.__config()
 
+
     def __help(self):
-        print('Help...')
+        print('usage: sss3 <command> [<args>]\n\nCommands:\n\tinit\tCreate an empty repository\n\tconfig\tView or set values for this repositories configuration')
         return
+
 
     def __init(self):
         self.__check_hidden_folder_exists()
-
         if len(self.arguments) == 5:
             #Insert data into json
-            data={"GUID":self.arguments[2],"Secret_Access_Key": self.arguments[3], "Access_Key_ID": self.arguments[4]}
-            with open(self.FOLDER_NAME, 'w') as outfile:
+            data={"GUID":self.arguments[2],"Secret_Access_Key": self.arguments[4], "Access_Key_ID": self.arguments[3]}
+            with open(self.CONFIG_FILE, 'w') as outfile:
                 json.dump(data, outfile)
 
-        elif len(self.arguments) == 3:
-            print("ahhdwq")
 
     def __status(self):
         return
@@ -99,34 +98,45 @@ class SSS3:
         return
 
     def __config(self):
-        if not os.path.isfile(self.FOLDER_NAME):
-            print "Configuration file does not exists, please run command init first"
+        if (not os.path.isfile(self.CONFIG_FILE)):
+            print "This directory is not a SSS3 archive!!!\nIf you would like to make it one run init command."
+            return
+
+        if len(self.arguments)<3:
+            print 'usage: sss3 config <argument> <argument> <argument>\n\nArguments:\n\tAWS\tPrints AWS credentials. Or sets credentials if new are provided.\n\tGUID\tPrint name of this repository.'
             return
 
         if self.arguments[2] == "GUID":
             if len(self.arguments) < 4:
-                print "GUID of directory is", self.config["GUID"]
+                print "GUID:", self.__read_config()["GUID"]
             else:
                 print "GUID is read only!!!"
             return
 
+
         if self.arguments[2] == "AWS":
+            if len(self.arguments)==3:
+                print 'AccessKeyID: \t{0}\nSecretAccessKey: \t{1}'.format(self.__read_config()["Access_Key_ID"], self.__read_config()["Secret_Access_Key"])
+                return
+
             if len(self.arguments) == 5:
                 dec=raw_input("Configuration already exists are you sure to overwrite it? Type y for yes and n for no")
                 if dec.startswith('y'):
-                    if self.__check_connection_to_bucket(self.arguments[3],self.arguments[4], self.config['GUID']):
-                        data = {"GUID": self.config['GUID'], "Secret_Access_Key": self.arguments[3], "Access_Key_ID": self.arguments[4]}
-                        with open(self.FOLDER_NAME, 'w') as outfile:
+                    if self.__check_connection_to_bucket(self.arguments[3],self.arguments[4], self.__read_config()['GUID']):
+                        data = {"GUID": self.__read_config()["GUID"], "Secret_Access_Key": self.arguments[3], "Access_Key_ID": self.arguments[4]}
+                        with open(self.CONFIG_FILE, 'w') as outfile:
                             json.dump(data, outfile)
                         print "Configuration saved!"
+                        return
                     else:
                         print "User doesn't have access to resource in AWS, exiting without saving..."
                 else:
                     print "Exiting without saving new configuration"
             else:
-                print "Invalid number of arguments!"
+                print "You need to specify both AccessKeyID and SecretAccessKey to change them!!!"
 
 
 if __name__ == '__main__':
     SSS3(sys.argv)
+
 
--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+import ctypes
 from StringIO import StringIO
 from sss3.sss3 import SSS3
 
@@ -12,7 +13,7 @@ class testConfigCommandWithNoConfigFile(unittest.TestCase):
 
   def test_no_arguments(self):
     SSS3(['sss3.py'])
-    self.assertEqual(sys.stdout.getvalue(), 'usage: sss3 <command> [<args>]\n\nCommands:\n\tinit\tCreate an empty repository\n\tconfig\tView or set values for this repositories configuration')
+    self.assertEqual(sys.stdout.getvalue(), 'usage: sss3 <command> [<args>]\n\nCommands:\n\tinit\tCreate an empty repository\n\tconfig\tView or set values for this repositories configuration\n')
 
   def test_no_parameters(self):
     SSS3(['sss3.py', 'config'])
@@ -55,17 +56,30 @@ class testWithConfigFileRestrectedAccess(unittest.TestCase):
     with open(self.TEST_FILE) as json_data:
       self.test_data = json.load(json_data)
 
-    data = {"GUID": self.test_data['names']['existing'], "Secret_Access_Key": self.test_data['AWS']['restrected']['AccessKeyID'], "Access_Key_ID": self.test_data['AWS']['restrected']['SecretAccessKey']}
-    with open(self.FOLDER_NAME, 'w') as outfile:
-      json.dump(data, outfile)
+    data = {"GUID": self.test_data['names']['existing'], "Secret_Access_Key": self.test_data['AWS']['restrected']['SecretAccessKey'], "Access_Key_ID": self.test_data['AWS']['restrected']['AccessKeyID']}
+
+    if not os.path.isdir(self.FOLDER_NAME):
+      if os.name == 'nt':
+        os.makedirs(self.FOLDER_NAME)
+        ctypes.windll.kernel32.SetFileAttributesW(ur"" + self.FOLDER_NAME, 0x02)
+      else:
+        os.makedirs("." + self.FOLDER_NAME)
+      open(self.CONFIG_FILE, 'w').close()
+    else:
+      if not os.path.isfile(self.CONFIG_FILE):
+        open(self.CONFIG_FILE, 'w').close()
+
+    if os.path.getsize(self.CONFIG_FILE)==0:
+      with open(self.CONFIG_FILE, 'w') as outfile:
+        json.dump(data, outfile)
 
   def test_no_arguments(self):
     SSS3(['sss3.py'])
-    self.assertEqual(sys.stdout.getvalue(), 'usage: sss3 <command> [<args>]\n\nCommands:\n\tinit\tCreate an empty repository\n\tconfig\tView or set values for this repositories configuration')
+    self.assertEqual(sys.stdout.getvalue(), 'usage: sss3 <command> [<args>]\n\nCommands:\n\tinit\tCreate an empty repository\n\tconfig\tView or set values for this repositories configuration\n')
 
   def test_config_no_parameters(self):
     SSS3(['sss3.py', 'config'])
-    self.assertEqual(sys.stdout.getvalue(), 'usage: sss3 config <argument> <argument> <argument>\n\nArguments:\n\tAWS\tPrints AWS credentials. Or sets credentials if new are provided.\n\tGUID\tPrint name of this repository.')
+    self.assertEqual(sys.stdout.getvalue(), 'usage: sss3 config <argument> <argument> <argument>\n\nArguments:\n\tAWS\tPrints AWS credentials. Or sets credentials if new are provided.\n\tGUID\tPrint name of this repository.\n')
 
   def test_config_print_GUID(self):
     SSS3(['sss3.py', 'config', 'GUID'])
@@ -81,7 +95,7 @@ class testWithConfigFileRestrectedAccess(unittest.TestCase):
 
   def test_config_print_AWS(self):
     SSS3(['sss3.py', 'config', 'AWS'])
-    self.assertEqual(sys.stdout.getvalue(), 'AccessKeyID: \t{0}\nSecretAccessKey: \t{1}'.format(self.test_data['AWS']['restrected']['AccessKeyID'], self.test_data['AWS']['restrected']['SecretAccessKey']))
+    self.assertEqual(sys.stdout.getvalue(), 'AccessKeyID: \t{0}\nSecretAccessKey: \t{1}\n'.format(self.test_data['AWS']['restrected']['AccessKeyID'], self.test_data['AWS']['restrected']['SecretAccessKey']))
 
   def test_config_set_AWS_not_enough_parameters(self):
     SSS3(['sss3.py', 'config', 'AWS', 'notEnough' ])
